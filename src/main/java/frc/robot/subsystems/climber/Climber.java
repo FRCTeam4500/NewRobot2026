c5679fa9044875c35ff1cbace0e94ebc0f174811
evan <evanheemstra2@gmail.com>
2026-01-16 14:40:59 -0600

    please fix your import issues

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.hardware.Motor;
 import frc.robot.hardware.Motor.TargetType;
