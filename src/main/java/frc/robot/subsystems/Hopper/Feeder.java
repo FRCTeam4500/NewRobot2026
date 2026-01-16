c1f5a8ce97d9c9973d9470ffb324d0c84338f6eb
evan <evanheemstra2@gmail.com>
2026-01-16 15:38:57 -0600

    subystme layout

---
+++ src/main/java/frc/robot/subsystems/Hopper/Feeder.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.Hopper;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utilities.logging.Loggable;
+
+public class Feeder extends SubsystemBase implements Loggable{
+
+    public Feeder(){
+
+        
+    }
+
+
+
+
+
+    @Override
+    public void log(String path) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'log'");
+    }
+
+
+
+
+
+    
+}
