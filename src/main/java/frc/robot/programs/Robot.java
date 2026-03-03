3015e74b0e05e3df02477eae7bd8c4b6d9938e91
evan <evanheemstra2@gmail.com>
2026-03-03 10:18:15 -0600

    auto work

--- src/main/java/frc/robot/programs/Robot.java
+++ src/main/java/frc/robot/programs/Robot.java
@@ -138,12 +138,14 @@ public class Robot extends LoggedRobot {
     NamedCommands.registerCommand("StartIntake", structure.StartIntake());
     NamedCommands.registerCommand("StopIntake", structure.StopIntake());
     NamedCommands.registerCommand("ExtendIntake", structure.ExtendIntake());
+    NamedCommands.registerCommand("RetractIntake", structure.RetractIntake());
     SmartDashboard.putData("Auto Chooser", chooser);
     chooser.addOption("center shoot/climb", new PathPlannerAuto("Auto 1"));
     chooser.addOption("left shoot/climb", new PathPlannerAuto("Auto 2a"));
     chooser.addOption("midle set", new PathPlannerAuto("Auto 4a"));
     chooser.addOption("2 midle cycle", new PathPlannerAuto("Auto 5a"));
     chooser.addOption("5 M auto", new PathPlannerAuto("New Auto"));
+    
 
     RobotModeTriggers.autonomous().whileTrue(Commands.deferredProxy(chooser::getSelected));
   }
